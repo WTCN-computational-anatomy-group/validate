1239ab76abddd6929ef3dbd5ffc50964283badfd
Mikael Brudfors <brudfors@gmail.com>
2020-01-24 13:44:47 +0000

    Added Model 5 (CROMIS)

--- diffeo-segment/GetModel.m
+++ diffeo-segment/GetModel.m
@@ -1,10 +1,11 @@
 function [P1,sett,model] = GetModel(model_num,P,ix,dir_res,opt)
 % MODELS:
 % 0. Testing
-% 1. Template prop MRBRAINS18, (K1=12), get CGM, SGM, WM, CSF, VEN
+% 1. Template prop MICCAI2012+MRBRAINS18, (K1=12), get CGM, SGM, WM, CSF, VEN
 % 2. Fit only T1 (K1=12)
 % 3. Template prop MRBRAINS18, (K1=8), get GM, WM, CSF
-% 4. Fit IXI (K1=12)
+% 4. Fit IXI (K1=14)
+% 5. CROMIS (K1=12)
 %__________________________________________________________________________
 % Copyright (C) 2019 Wellcome Trust Centre for Neuroimaging
 
@@ -28,12 +29,13 @@ N      = min(N,numsubj);
 int_ix = [1 2];
 
 % Number of template classes
-K = 5;
+K  = 5;
+K1 = K + 1;
 
 % Label information
-% icgm   = 1; isgm = 2; iwm = 3; icsf = 4; iven = 5; k1 = K + 1;
-% cm_map = {{icgm,isgm,[isgm iwm],iwm,iven, setdiff(1:k1,[icgm isgm iven])}, ...
-%           {icgm,isgm,[isgm iwm],iwm,[isgm iwm],icsf,iven,setdiff(1:k1,[icgm isgm iven])}, ...
+% icgm   = 1; isgm = 2; iwm = 3; icsf = 4; iven = 5;
+% cm_map = {{icgm,isgm,[isgm iwm],iwm,iven, setdiff(1:K1,[icgm isgm iven])}, ...
+%           {icgm,isgm,[isgm iwm],iwm,[isgm iwm],icsf,iven,setdiff(1:K1,[icgm isgm iven])}, ...
 %           {iwm,[]}};
 cm_map = cell(1,numel(ix_pop));
 
@@ -63,7 +65,7 @@ if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % cl
 end
 
 %%%%%%%%%%%%%%%%%%%
-% Model 1 | Template prop MRBRAINS18, (K1=12), get CGM, SGM, WM, CSF, VEN
+% Model 1 | Template prop MICCAI2012+MRBRAINS18, (K1=12), get CGM, SGM, WM, CSF, VEN
 %------------------
 if model_num == 1, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
 
@@ -74,12 +76,13 @@ N       = min(N,numsubj);
 int_pop = [1 1 2 3 4];
 
 % Number of template classes
-K = 11;
+K  = 11;
+K1 = K + 1;
 
 % Label information
-icgm   = 1; isgm = 2; iwm = 3; icsf = 4; iven = 5; k1 = K + 1;
-cm_map = {{icgm,isgm,[isgm iwm],iwm,icsf,iven, setdiff(1:k1,[icgm])}, ...
-          {icgm,isgm,[isgm iwm],iwm,[isgm iwm],icsf,iven,setdiff(1:k1,[icgm])}, ...
+icgm   = 1; isgm = 2; iwm = 3; icsf = 4; iven = 5;
+cm_map = {{icgm,isgm,[isgm iwm],iwm,icsf,iven, setdiff(1:K1,[icgm])}, ...
+          {icgm,isgm,[isgm iwm],iwm,[isgm iwm],icsf,iven,setdiff(1:K1,[icgm])}, ...
           {[isgm iwm],[]},{},{}};
 % cm_map = cell(1,numel(ix_pop));
 
@@ -118,6 +121,10 @@ ixs    = [ix.IXI  ix.MICCAI2012 ix.BALGRIST];
 N      = [150 20 10]; % Set maximum number of subjects
 N      = min(N,numsubj);
 
+% Number of template classes
+K  = 11;
+K1 = K + 1;
+
 % Define training population
 P1 = P(ixs);
 for p=1:numel(P1)    
@@ -128,14 +135,14 @@ end
 
 % Settings
 sett                    = struct;
-sett.show.figs          = {'model','segmentations'};
+sett.show.figs          = {'model','segmentations','InitGMM'};
 sett.model.init_mu_dm   = 16;  
 sett.write.intermediate = true;
 sett.write.clean_vel    = false;
 sett.write.dir_res      = fullfile(dir_res,['results/model-' num2str(model_num)]);
 if ~run3d, sett.write.dir_res = [sett.write.dir_res '-2D-' ax2d]; end
 sett.labels.use         = false; 
-sett.model.K            = 11;  
+sett.model.K            = K;  
 sett.write.mu           = [true true];
 if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % clear results directory
 end
@@ -188,12 +195,16 @@ if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % cl
 end
 
 %%%%%%%%%%%%%%%%%%%
-% Model 4 | Fit IXI (K1=12)
+% Model 4 | Fit IXI (K1=14)
 %------------------
 if model_num == 4, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
 
 N = 100; % Set maximum number of subjects
 N = min(N,numsubj);
+
+% Number of template classes
+K  = 13;
+K1 = K + 1;
 
 % Define training population
 P1       = P(ix.IXI);  
@@ -209,10 +220,50 @@ sett.write.clean_vel    = false;
 sett.write.dir_res      = fullfile(dir_res,['results/model-' num2str(model_num)]);
 if ~run3d, sett.write.dir_res = [sett.write.dir_res '-2D-' ax2d]; end
 sett.labels.use         = false; 
-sett.model.K            = 11;  
+sett.model.K            = K;  
 sett.write.mu           = [true true];
 sett.model.crop_mu      = false;
 if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % clear results directory
 end
+
+%%%%%%%%%%%%%%%%%%%
+% Model 5 | CROMIS (K1=12)
+%------------------
+if model_num == 5, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
+
+% Define training population
+ix_pop = [ix.CROMISLABELS ix.CROMIS];
+N      = Inf*ones(1,numel(ix_pop)); % Set maximum number of subjects
+N      = min(N,numsubj);
+
+% Number of template classes
+K  = 11;
+K1 = K + 1;
+ 
+% Label information
+iles   = 1;
+cm_map = {{iles, setdiff(1:K1,iles)}, {}};
+
+P1 = P(ix_pop);
+for p=1:numel(P1)
+    P1{p}{3} = N(p); 
+    P1{p}{4} = 1;      
+    P1{p}{5} = cm_map{p};
+end
+
+% Settings
+sett                    = struct;
+sett.show.figs          = {'model','segmentations','InitGMM','intensity'};
+sett.write.dir_res      = fullfile(dir_res,['results/model-' num2str(model_num)]);
+if ~run3d, sett.write.dir_res = [sett.write.dir_res '-2D-' ax2d]; end
+sett.write.intermediate = true;
+sett.model.mg_ix        = 1;
+sett.labels.use         = true; 
+sett.model.K            = K;  
+sett.show.mx_subjects   = 4;
+sett.write.mu           = [true true];
+if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % clear results directory
+end
+
 end
 %==========================================================================
