079e49233f4675df2ded9454b144b7cf8e5907d8
Mikael Brudfors <brudfors@gmail.com>
2020-03-12 18:43:49 +0000

    Mods to MRI+CT model

--- diffeo-segment/GetModel.m
+++ diffeo-segment/GetModel.m
@@ -3,12 +3,11 @@ function [P1,sett,model] = GetModel(model_num,P,ix,dir_res,opt)
 % 0. Testing
 % 1. Template prop MICCAI2012+MRBRAINS18, (K1=12), get CGM, SGM, WM, CSF, VEN
 % 2. Fit T1w (K1=12)
-% 3. Template prop MRBRAINS18, (K1=8), get GM, WM, CSF
-% 4. Fit T1w,T2w,PDw (K1=12)
-% 5. CROMIS (K1=12)
-% 6. MICCAI2020 (IBSR18)
-% 7. MICCAI2020 (LPBA40)
-% 8. MICCAI2012
+% 3. MRI+CT model (K1=12)
+% 4. CROMIS (K1=12)
+% 5. MICCAI2020 (IBSR18)
+% 6. MICCAI2020 (LPBA40)
+% 7. MICCAI2012
 %__________________________________________________________________________
 % Copyright (C) 2019 Wellcome Trust Centre for Neuroimaging
 
@@ -153,114 +152,66 @@ sett.labels.use         = false;
 sett.model.K            = K;  
 sett.model.crop_mu      = false;
 sett.model.init_mu_dm   = 8;
-sett.nit.init           = 12;
+sett.nit.init           = 32;
 sett.var.v_settings     = [0 0 0.2 0.05 0.2]*2;
 if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % clear results directory
 end
 
 %%%%%%%%%%%%%%%%%%%
-% Model 3 | Template prop MRBRAINS18, (K1=8), get GM, WM, CSF
+% Model 3 | MRI+CT model (K1=12)
 %------------------
 if model_num == 3, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
 
 % Define training population
-ix_pop  = [ix.IXI ix.MRBRAINS18 ix.MICCAI2012 ix.BALGRIST ix.ROB];
-N       = 100*ones(1,numel(ix_pop)); % Set maximum number of subjects
+ix_pop  = [ix.BALGRIST ix.IXI ix.MICCAI2012 ix.MRBRAINS18 ix.CTHEALTHY ix.ROB]; 
+N       = [Inf 300 Inf Inf Inf Inf]; 
 N       = min(N,numsubj); 
-int_pop = [1 2 2 3 4];
-
-% Number of template classes
-K = 9; K1 = K + 1;
-
-% Label information
-igm = 9; iwm = 4; icsf = 6;
-cm_map = {{},{igm,igm,[igm iwm],iwm,[igm iwm],1:K1,setdiff(1:K1,[igm iwm]),1:K1}, ...
-          {igm,igm,[igm iwm],iwm,1:K1,setdiff(1:K1,[igm iwm]),1:K1}, ... 
-          {[igm iwm],[]},...                   
-          {}};
-% cm_map = cell(1,numel(ix_pop));
-
-P1 = P(ix_pop);
-for p=1:numel(P1)
-    P1{p}{3} = N(p);
-    P1{p}{4} = int_pop(p);  
-    P1{p}{5} = cm_map{p};
-end
-if numel(P1) >= 1
-    P1{1}{2} = {'T1','T2','PD'};
-end
-
-% Settings
-sett                    = struct;
-if showfig, sett.show.figs = {'model','segmentations'}; end
-sett.gen.num_workers    = nw;
-sett.write.dir_res      = fullfile(dir_res,['results/model-' num2str(model_num)]);
-if ~run3d, sett.write.dir_res = [sett.write.dir_res '-2D-' ax2d]; end
-sett.write.workspace    = true;
-sett.write.affine       = true;
-sett.write.vel          = true;
-sett.write.mu           = [true true];
-sett.labels.use         = true; 
-sett.model.K            = K;  
-sett.model.ix_init_pop  = 1;
-sett.show.mx_subjects   = 8;
-sett.model.mg_ix        = 1;%[1 2 3 3 4 4 4 5 5 5 6 6 6];
-sett.model.crop_mu      = false;
-if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % clear results directory 
-end
-
-%%%%%%%%%%%%%%%%%%%
-% Model 4 | Fit T1w,T2w,PDw (K1=12)
-% Less classes? Two CSF. Bone labelled from CT.
-%------------------
-if model_num == 4, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
-
-% Define training population
-ix_pop  = [ix.BALGRIST ix.IXI ix.MICCAI2012 ix.MRBRAINS18 ix.CTHEALTHY];
-N       = [Inf 200 Inf Inf Inf];
-N       = min(N,numsubj); 
-int_pop = [1 2 3 3 4];
+int_pop = [1 2 3 3 4 4]; 
 
 % Number of template classes
 K = 11; K1 = K + 1;
 
-igm = 12; iwm = 9; icsf = 11;
-cm_map = {{[igm iwm],1:K1},... 1.spn
+igm = 12; iwm = 9; icsf = 11; ibg = 1; ibn = 10;
+cm_map = {{[igm iwm],1:K1},... 1.spn 
           {}, ...
-          {igm,igm,[igm iwm],iwm,icsf,icsf,1:K1}, ... % 1.cgm,2.sgm,3.spn,4.wm,5,csf,6.ven 
-          {igm,igm,[igm iwm],iwm,[igm iwm],icsf,icsf,1:K1}, ...  % 1.cgm,2.sgm,3.spn,4.wm,5.cer,6.csf,7.ven                                    
+          {igm,igm,[igm iwm],iwm,icsf,icsf,1:K1}, ... % 1.cgm,2.sgm,3.spn,4.wm,5,csf,6.ven  
+          {igm,igm,[igm iwm],iwm,[igm iwm],icsf,icsf,1:K1}, ...  % 1.cgm,2.sgm,3.spn,4.wm,5.cer,6.csf,7.ven                                     
+          {ibg,ibn,setdiff(1:K1,ibn)}, ...  % 1.bg,2.bone
           {}};
-      
+          
 P1 = P(ix_pop);
 for p=1:numel(P1)
     P1{p}{3} = N(p);
     P1{p}{4} = int_pop(p);  
     P1{p}{5} = cm_map{p};
 end
-P1{2}{2} = {'T1','T2','PD'};
+P1{2}{2} = {'T1','T2','PD'}; 
 
 % Settings
 sett                    = struct;
-if showfig, sett.show.figs = {'model','segmentations'}; end
+if showfig, sett.show.figs = {'model','segmentations','init','intensity'}; end
 sett.gen.num_workers    = nw;
 sett.write.dir_res      = fullfile(dir_res,['results/model-' num2str(model_num)]);
 if ~run3d, sett.write.dir_res = [sett.write.dir_res '-2D-' ax2d]; end
-sett.write.df           = true;
+sett.write.df           = false;
 sett.write.mu           = [true true];
 sett.labels.use         = true; 
 sett.labels.use_initgmm = false;
 sett.model.K            = K;  
 sett.model.ix_init_pop  = 1;
 sett.model.crop_mu      = false;
-sett.nit.init           = 12;
+sett.nit.init           = 32;
+sett.nit.zm             = 4;
+sett.model.mg_ix        = 1;
+sett.model.init_mu_dm   = 16;
 sett.var.v_settings     = [0 0 0.2 0.05 0.2]*4;
 if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % clear results directory
 end
 
 %%%%%%%%%%%%%%%%%%%
-% Model 5 | CROMIS (K1=12)
+% Model 4 | CROMIS (K1=12)
 %------------------
-if model_num == 5, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
+if model_num == 4, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
 
 % Define training population
 ix_pop = [ix.CROMISLABELS ix.CROMIS];
@@ -300,9 +251,9 @@ if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % cl
 end
 
 %%%%%%%%%%%%%%%%%%%
-% Model 6 | MICCAI2020 (IBSR18)
+% Model 5 | MICCAI2020 (IBSR18)
 %------------------
-if model_num == 6, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
+if model_num == 5, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
 % Set training populations to use
 ixs = ix.IBSR18;
 N   = 18;
@@ -331,9 +282,9 @@ if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % cl
 end
 
 %%%%%%%%%%%%%%%%%%%
-% Model 7 | MICCAI2020 (LPBA40)
+% Model 6 | MICCAI2020 (LPBA40)
 %------------------
-if model_num == 7, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
+if model_num == 6, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
 % Set training populations to use
 ixs = ix.LPBA40;
 N   = 40;
@@ -362,9 +313,9 @@ if exist(sett.write.dir_res,'dir') == 7, rmdir(sett.write.dir_res,'s'); end % cl
 end
 
 %%%%%%%%%%%%%%%%%%%
-% Model 8 | MICCAI2012
+% Model 7 | MICCAI2012
 %------------------
-if model_num == 8, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
+if model_num == 7, fprintf('=============\nMODEL %i\n=============\n\n',model_num);
 % Set training populations to use
 ixs = ix.MICCAI2012;
 N   = 20;
